3782bc345ec72f39b64dddfce44248b706da7e48
Hamza Waris <hamzawarisgt@gmail.com>
2024-03-20 06:25:53 -0700

    msg

--- src/server.java
+++ src/server.java
@@ -71,7 +71,7 @@ public class Server implements ActionListener {
         morevert.setBounds(420, 20, 10, 25);
         p1.add(morevert);
 
-        JLabel name = new JLabel("Gaitonde");
+        JLabel name = new JLabel("vishal");
         name.setBounds(110, 15, 100, 18);
         name.setForeground(Color.WHITE);
         name.setFont(new Font("SAN_SERIF", Font.BOLD, 18));
